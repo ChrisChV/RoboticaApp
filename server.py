a1c052231814c68431aa1e8423ed2605315e2cae
ChrisChV <cristo2014f@outlook.es>
2018-12-02 23:58:33 -0500

    server

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ import urllib
 import cv2
 import numpy as np
 import time
+from keras.models import load_model
 
 # Replace the URL with your own IPwebcam shot.jpg IP:port
-url='http://192.168.0.46:8080/shot.jpg'
+url='http://192.168.8.103:8080/shot.jpg'
 
+model = load_model('model.h5')
 
 while True:
     # Use urllib to get the image from the IP camera
@@ -15,13 +17,20 @@ while True:
     imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
     
     # Finally decode the array to OpenCV usable format ;) 
-    img = cv2.imdecode(imgNp,-1)
+    img = cv2.resize(imgNp, dsize=(120, 160), interpolation=cv2.INTER_CUBIC)
+    print("shape", img.shape)
+    P = model.predict(img)
+    print(P)
+
+    time.sleep(0.6)
+    
+    img = cv2.imdecode(imgNp, -1)
 	
 	# put the image on screen
     cv2.imshow('IPWebcam',img)
 
     #To give the processor some less stress
-    #time.sleep(0.1) 
+    
 
     # Quit if q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
